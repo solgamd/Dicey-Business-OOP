68251e27090096efddbc64e03405db1326b5979e
solgamd <solgamd@gmail.com>
2019-05-05 20:58:46 +0200

    Refactoring with DOM manipulation

--- app.js
+++ app.js
@@ -1,79 +1,76 @@
-// const MAX = 800;
+// $('.btn').click(function () {    // BUTTON FUN
+//     let colors = ['blue', 'teal', 'turquoise', 'navy', 'darkred'];
+//     let newColor = colors[Math.floor(Math.random() * colors.length)];
+//         if ($(this).css('color', 'black')) {
+//             $(this).css('color', 'white')
+//         };
+//         this.style.backgroundColor = newColor;
+// });
+// function btnRoll(min, max) {       ////////// WRONG CODE ---------------------->
+//     this.value = Math.random() * (max - min) + min;
+//     ('#die').append(div);
+// }
 
-// let container = document.getElementById('square-container');
-// class Square {
-//     constructor(x, y) {
-//         this.div = document.createElement('div');
-//         this.div.classList.add('square');
-//         this.div.style.left = `${x}px`;
-//         this.div.style.top = `${y}px`;
-//         this.div.addEventListener('click', () => {
-//             this.updateColor();
-//             this.updateLocation();
-//         });
-//         this.updateColor();
-//         container.appendChild(this.div);
+//// ----------------------------------------------------------------------------------
+// class Die {
+//     constructor() {
+//         // this.value = value;
+
+//         $('#btnDie').click(function () {    //GENERATE DIE
+//             let div = '<div id="die" class="col-3"></div>';
+
+//             div.id = 'die';
+//             console.log('clicked');
+//             roll();
+//         })
+
 //     }
-
-//     updateLocation() {
-//         let xVal = randomVal(0, MAX);
-//         let yVal = randomVal(0, MAX);
-//         this.div.style.left = `${xVal}px`;
-//         this.div.style.top = `${yVal}px`;
-//     }
-
-//     updateColor() {
-//         let randomColor = `rgb(${randomVal(0, 255)}, ${randomVal(0, 255)}, ${randomVal(0, 255)})`;
-//         this.div.style.backgroundColor = randomColor;
+//     roll(min, max) {
+//         this.min = 1;
+//         this.max = 6;
+//         this.value = Math.floor(Math.random() * (max - min) + min);
+//         console.log(this.value);
+//         // $('#dieDiv').append(div);
 //     }
 // }
 
-// let sqButton = document.getElementById('sq-button');
-// sqButton.addEventListener('click', insertSquare);
+// --------------------------------------------------------------------------
 
-// function insertSquare() {
-//     let xVal = randomVal(0, MAX);
-//     let yVal = randomVal(0, MAX);
-//     let sq = new Square(xVal, yVal);
+
+// function roll() {
+//     let min = 1;
+//     let max = 6;
+//     const value = Math.floor(Math.random() * (max - min) + min);
+//     die.innerText = value;
+
+//     console.log(value);
 // }
+let btnDie = document.getElementById('btnDie');
 
-// function randomVal(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-// }
-
-
-
-
-
-//////////////
-
-$('#btnDie').click(function () {    //GENERATE DIE
-    let div = '<div id="die" class="col-3"></div>';
-    div.id = 'die';
-    $('#dieDiv').append(div);
-})
-
-$('.btn').click(function () {    // BUTTON FUN
-    let colors = ['blue', 'teal', 'turquoise', 'navy', 'sea foam', 'darkred'];
-    let newColor = colors[Math.floor(Math.random() * colors.length)];
-        this.style.backgroundColor = newColor;
-        $(this).css('color', 'white');
+btnDie.addEventListener('click', function () {
+    let die = document.createElement('div');
+    let dieCont = document.getElementById('dieContainer');
+    dieCont.appendChild(die);
+    die.id = 'die';
+    console.log('die created');
+    // this.value = value;
 });
-
-function btnRoll(min, max) {
-    this.value = Math.random() * (max - min) + min;
-}
-
 class Die {
-        constructor (value){
-            this.value = value;
-            this.div.addClass = 'die';
+    constructor() {
+
 
     }
-    roll(min, max) {
-        this.value = Math.random() * (max - min) + min;
-    }
+    // roll() {
+    //     let min = 1;
+    //     let max = 6;
+    //     let value = Math.floor(Math.random() * (max - min) + min);
+    //     die.innerText = value;
+    //     console.log('rolled');
+    // }
 }
 
-
-// let newDie = new Die(6, 1)
+// let btnRoll = document.getElementById('btnRoll');
+// btnRoll.addEventListener('click', function () {
+//     let allBtns = document.getElementsByClassName('.btn');
+//     allBtns.innerText.roll();
+// })
